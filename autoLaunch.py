72f04230c0903cdae22f028b35bcb5630274e92e
emjayking <emjayking23@hotmail.co.nz>
2018-11-26 13:42:12 +1300

    fixing merge conflicts

---
+++ autoLaunch.py
@@ -0,0 +1,68 @@
+#this is an experiment with the auto launch on feature
+
+from pyparrot.Minidrone import Mambo
+
+mamboAddr = "e0:14:bc:c5:3d:cb"
+mambo = Mambo(mamboAddr, use_wifi=False)
+
+print("trying to connect")
+success = mambo.connect(num_retries=3)
+print("connected: %s" % success)
+
+if (success):
+    print("getting updates")
+    mambo.smart_sleep(2)
+    mambo.ask_for_state_update()
+    mambo.smart_sleep(2)
+
+    print("ready for takeoff!")
+    mambo.turn_on_auto_takeoff()
+    print(mambo.sensors.flying_state)
+    mambo.smart_sleep(10)
+    while True:
+        print(mambo.sensors.flying_state)
+        if mambo.sensors.flying_state !=  "landed":
+            print("flying forward")
+            mambo.smart_sleep(3)
+            mambo.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=1)
+
+            print("flying backwards")
+            mambo.smart_sleep(2)
+            mambo.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=1)
+
+            print("turning 360")
+            mambo.smart_sleep(2)
+            mambo.turn_degrees(180)
+            mambo.smart_sleep(2)
+            mambo.turn_degrees(180)
+
+            print("flying 'yaw'")
+            mambo.smart_sleep(2)
+            mambo.fly_direct(roll=0, pitch=0, yaw=50, vertical_movement=0, duration=1)
+
+            print("flying negative yaw")
+            mambo.smart_sleep(2)
+            mambo.fly_direct(roll=0, pitch=0, yaw=-50, vertical_movement=0, duration=1)
+
+            print("flying roll")
+            mambo.smart_sleep(2)
+            mambo.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0, duration=1)
+
+            print("flying negative roll")
+            mambo.smart_sleep(2)
+            mambo.fly_direct(roll=-50, pitch=0, yaw=0, vertical_movement=0, duration=1)
+
+            print("goin up!")
+            mambo.smart_sleep(2)
+            mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=50, duration=1)
+
+            print("goin down")
+            mambo.smart_sleep(2)
+            mambo.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-50, duration=1)
+
+            print("landing")
+            mambo.safe_land(5)
+            mambo.smart_sleep(5)
+
+            print("disconnect")
+            mambo.disconnect()
